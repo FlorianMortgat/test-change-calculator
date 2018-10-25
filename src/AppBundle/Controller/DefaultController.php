9e60c6900e6de41510d7d2d9710b084f52c57e48
FlorianMortgat <5845502+FlorianMortgat@users.noreply.github.com>
2018-10-25 19:59:06 +0200

    Completed the controller to ensure correct routing

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,7 +5,38 @@ declare(strict_types=1);
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+//use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Registry\CalculatorRegistry;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/automaton/{model}/change/{amount}")
+     */
+    public function calculateChange(string $model, int $amount)
+    {
+        $calculatorRegistry = new CalculatorRegistry();
+        $calculator = $calculatorRegistry->getCalculatorFor($model);
+        if (is_null($calculator)) {
+            //throw new NotFoundHttpException('The model does not exist.');
+            return new Response(
+                'The model does not exist.',
+                404
+            );
+        }
+        $change = $calculator->getChange($amount);
+        if (!$change) {
+            return new Response(
+                '',
+                204
+            );
+        }
+
+        return new Response(
+            json_encode($change),
+            200
+        );
+    }
 }
