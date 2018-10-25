8825e1a5a8082a6d2cc7fa9e02accd2b40d32c87
FlorianMortgat <5845502+FlorianMortgat@users.noreply.github.com>
2018-10-25 19:52:41 +0200

    Fixed GenericCalculator to get a correct result in any case

--- src/AppBundle/Calculator/GenericCalculator.php
+++ src/AppBundle/Calculator/GenericCalculator.php
@@ -17,6 +17,7 @@ class GenericCalculator implements CalculatorInterface
      */
     protected $availableCurrencies = [];
     protected $model;
+    private $MAX_BACKTRACKS = 40;
 
     /**
      * @return string the name of the model
@@ -36,20 +37,67 @@ class GenericCalculator implements CalculatorInterface
      */
     public function getChange(int $amount): ?Change
     {
-        if ($amount < 0) {
+        if (0 == $amount) {
+            return new Change();
+        } elseif ($amount < 0) {
             return null;
-        } else {
-            $change = new Change();
-            foreach ($this->availableCurrencies as $currencyName => $currencyValue) {
-                $change->$currencyName = intdiv(
-                    $amount,
-                    $currencyValue
-                );
-                $amount %= $currencyValue;
+        }
+        $change = new Change();
+        $availableCurrencies = $this->availableCurrencies;
+        if (!count($availableCurrencies)) {
+            throw new \Exception('The model doesn\'t hold any currency.');
+        }
+        // make sure the available currencies are sorted from highest to lowest value
+        arsort($availableCurrencies);
+        $currencies = array_values($availableCurrencies);
+        $currency_names = array_keys($availableCurrencies);
+        $num_currencies = count($currencies);
+        $factors = array_fill(0, $num_currencies, 0);
+        $remainders = array_fill(0, $num_currencies, 0);
+
+        $no_solution = true;
+
+        $safeguard_counter = 0;
+        $n = 0;
+
+        while ($safeguard_counter++ < $this->MAX_BACKTRACKS) {
+            echo "\$n = $n\n";
+            for (; $n < $num_currencies; $n++) {
+                $factors[$n] = intdiv($amount, $currencies[$n]);
+                $remainders[$n] = $amount % $currencies[$n];
+                $amount = (int) $remainders[$n];
+            }
+            $n--;
+
+            if (!$amount) {
+                // no remainder = success: put the results into the
+                // Change object and return it
+                for ($n = 0; $n < $num_currencies; $n++) {
+                    $currency_name = $currency_names[$n];
+                    $change->$currency_name = $factors[$n];
+                }
+
+                return $change;
             }
 
-            return $amount ? null : $change;
+            // remove some higher-value currencies (starting from the
+            // smallest) and retry dividing, excluding the removed currencies
+            $n_ = $n - 1;
+            if ($n_ < 0) {
+                return null;
+            }
+            for (; $n_ >= 0 && 0 == $factors[$n_]; $n_--) {
+            }
+            if ($n_ < 0) {
+                return null;
+            }
+            $factors[$n_]--;
+            $remainders[$n_] += $currencies[$n_];
+            $amount = (int) $remainders[$n_];
+            $n = $n_ + 1;
         }
+        throw new \Exception("Backtracking exceeded $this->MAX_BACKTRACKS.");
+        
     }
 }
 
